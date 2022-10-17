d3e68eac6d24976d999659d7649376ed145a4f7b
LI-MSU <sarr.mawade@gmail.com>
2022-10-17 16:57:07 -0700

    KILOL

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+
+/*Yo this is about that
+ *Might chekc this
+ */
 int main(void)
+/*Just do it
+*skdoksodk 
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*Bimbam*/
+int main()
+/*What this does is print the contained text*/
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
