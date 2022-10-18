0f87e0e2bb2f25553861804eb66a18bf17a8a7a5
LI-MSU <sarr.mawade@gmail.com>
2022-10-17 18:23:49 -0700

    lOL

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  char alphabet;
+  
+  char bet_upper;
+  
+
+  
+  for(alphabet = 'a';alphabet <= 'z';alphabet++)
+    
+    {
+      
+      putchar(alphabet);
+      
+    }
+  
+  for(bet_upper = 'A';bet_upper <= 'Z';bet_upper++)
+    
+    {
+      
+      putchar(bet_upper);
+      
+    }
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
