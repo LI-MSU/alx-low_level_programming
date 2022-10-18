e21a426dd79832bdb42bcebb68f69af60b60943a
LI-MSU <sarr.mawade@gmail.com>
2022-10-17 17:18:06 -0700

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-/*Bimbam*/
-int main(void)
-/*What this does is print the contained text*/
-{
-  printf ("with proper grammar, but the outcome is a piece of art,");
-  return (0);
+#include <stdio.h> /* need one here ? */
+
+int main(void) /* This is the maint thing */
+
+{ /* maybe hgere */
+/* kkkk */
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
