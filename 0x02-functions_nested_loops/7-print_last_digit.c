48e894828521497c165f2a07f0d17cdaf749a4a8
LI-MSU <sarr.mawade@gmail.com>
2022-10-19 18:15:50 -0700

    l

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,17 @@
 
 int print_last_digit(int number)
 {
-  int dig;
-  
-  dig = number % 10;
-  if (dig < 0)
-    {
-      _putchar(-dig + '0');
-      return (-dig); 
-    }
-  else
-    {
-      _putchar(dig + '0');
-      return (dig);
-      
-    }
+int dig;
+
+dig = number % 10;
+if (dig < 0)
+{
+  _putchar(-dig + '0');
+return (-dig);
+}
+else
+{
+_putchar(dig + '0');
+return (dig);
+}
 }
