0c341ef16bf2428966b37896ac92d6fe86df7112
LI-MSU <sarr.mawade@gmail.com>
2022-10-19 17:07:08 -0700

    ;

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign-> This will print the sign of the element.
+ * @n: is just another character arg
+ * With ifs and elses we can change tej world
+ */
+
+int print_sign(int n)
+{
+	if
+	{ n > 0;
+		printf('+');
+	}
+	else
+	{ n = 0;
+		printf('0');
+	}
+	else
+	{
+		n < 0;
+		printf('-')
+	}
+}
