1faa004b2aa2f1efa93fec7b8eaa452e15a843de
LI-MSU <sarr.mawade@gmail.com>
2022-10-25 16:22:47 -0700

    l

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
+ * reset_to_98 - Will set the value of *n to 98
  *
- *
+ * @n: is a pointer
  */
 
 void reset_to_98(int *n)
 {
-	int n;
-	int *pPointer = &n;
 
-	*pPointer = 98;
+	*n = 98;
 }
