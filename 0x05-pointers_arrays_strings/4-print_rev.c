0020c0cdcf301ca9110bbbaf1de13128c1892d3d
LI-MSU <sarr.mawade@gmail.com>
2022-10-25 18:30:08 -0700

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - will print reversed string + \n
+ * @b: a variable
+ * @s: the pointer
+ */
+
+void print_rev(char *s)
+{
+	int b = 0;
+	while (*(s + b) != '\0')
+	{
+		revstr(*s);
+		putchar(*(s + b));
+		b++;
+	}
+	putchar(10);
+}
