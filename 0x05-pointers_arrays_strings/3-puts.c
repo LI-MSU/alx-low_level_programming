4690bc799d36b94e25fe10767c49a95079ad429b
LI-MSU <sarr.mawade@gmail.com>
2022-10-25 17:49:37 -0700

    l

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - is function that increments str in order to print it
- * @str: is a pointer
- */
+  * _puts - Prints the string if it contains something +\0
+  * @str: is a string
+  * Return: void
+  */
 
 void _puts(char *str)
 {
-	for (*str = 0 ; *str != '\0' ; str++)
+	for (; *str != '\0'; str++)
+	{
 		_putchar(*str);
+	}
 	_putchar('\n');
 }
