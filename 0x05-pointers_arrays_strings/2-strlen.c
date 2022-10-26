62bc8c00cb42c9ef5c03c772271ad536da8f4329
LI-MSU <sarr.mawade@gmail.com>
2022-10-25 17:01:14 -0700

    ;

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen(char *s) - function that returns length of string.
+ * _strlen - function that returns length of string.
  * @s: this is a pointer.
+ * @i: this is a variable called to mimick value of *s
  * Return - will print the result
  */
 
 int _strlen(char *s)
 {
-	*s = "hello world";
-	return (strlen(s));
+	int i = 0;
+	while (*s++);
+	i++;
+	return (i);
 }
