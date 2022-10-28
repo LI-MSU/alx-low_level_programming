af0dec478987988dc15c1b9c896f18037e3c266c
LI-MSU <sarr.mawade@gmail.com>
2022-10-27 18:51:42 -0700

    ;

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@ char *_strcat(char *dest, char *src)
 {
 	int len1, len2, i;
 
-	len1 = strlen(*dest);
-	len2 = strlen(*src);
+	len1 = strlen(dest);
+	len2 = strlen(src);
 
 	for (i = 0; i <= len2; i++)
 	{
-		*dest[len1 + i] = *src [i];
+		dest[len1 + i] = src [i];
 	}
 
-	return (*dest);
+	return (dest);
 
 }
