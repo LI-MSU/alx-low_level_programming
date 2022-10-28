779ce52c57b64a1fc7742a91a0ced2b20a6e03a4
LI-MSU <sarr.mawade@gmail.com>
2022-10-27 19:30:39 -0700

    ;

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\n')
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
