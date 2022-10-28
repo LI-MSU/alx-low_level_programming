b00c9b4c9b170a3efd36d052d9e25126a523a9de
LI-MSU <sarr.mawade@gmail.com>
2022-10-27 19:19:36 -0700

    ;

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,5 +8,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int len1, len2, i;
 
+	len1 = strlen (dest);
+	len2 = strlen (src);
+
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src [n];
+	}
+	return (dest);
 }
